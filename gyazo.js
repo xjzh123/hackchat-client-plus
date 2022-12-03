fdccbb7eeefb13b2b73cba6970050e8b48dc249c
xjzh123 <37945610+xjzh123@users.noreply.github.com>
2022-12-03 21:55:27 +0800

    test cat box

---
+++ gyazo.js
@@ -0,0 +1,14 @@
+function getfile() {
+    file = document.getElementById('gyazo-upload').files
+
+    fetch('https://catbox.moe/user/api.php', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ reqtype:"fileupload", fileToUpload:file })
+    })
+    .then(response => response.json())
+    .then(response => console.log(JSON.stringify(response)))
+}
